fa8291d7d277a5ce2b93769285e4b81d4b90ef4a
aaa <834320733@qq.com>
2022-02-04 20:26:22 +0800

    Change Hi to allow multilang

--- say.go
+++ say.go
@@ -1,7 +1,21 @@
 package gomodone
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func SayHi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+func SayHi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("!Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default :
+		return "", errors.New("unknown language")
+	}
 }
